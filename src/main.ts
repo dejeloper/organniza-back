6d769683228646d035b4499ae4793ef1987d1e8f
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-02-16 11:57:34 -0500

    feat: Add Swagger documentation setup to the application

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import {NestFactory} from '@nestjs/core';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
@@ -19,6 +20,15 @@ async function bootstrap() {
     }
     next();
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentación')
+    .setDescription('Documentación de la API con Swagger')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
