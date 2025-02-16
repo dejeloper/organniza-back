b0f440ae3900fc90d1f08b0b8ae90417a10936ce
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-02-16 11:11:12 -0500

    feat: Update API endpoints and add global CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,23 @@ import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api');
+
+  app.use((req, res, next) => {
+    if (req.originalUrl === '/') {
+      return res.redirect(301, '/api');
+    }
+    next();
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
